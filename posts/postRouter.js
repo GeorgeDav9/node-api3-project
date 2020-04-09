86d2f61eb24014cb6f1de3420a9e98e74d85aa2c
George Davila <georgedavila@Georges-MacBook-Pro.local>
2020-04-08 23:20:52 -0400

    finished mvp

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,30 @@
 const express = require('express');
 
+const Posts = require('./postDb.js');
+
 const router = express.Router();
 
+// GET all posts
 router.get('/', (req, res) => {
   // do your magic!
 });
 
+// GET
 router.get('/:id', (req, res) => {
   // do your magic!
 });
 
+// DELETE
 router.delete('/:id', (req, res) => {
   // do your magic!
 });
 
+// UPDATE
 router.put('/:id', (req, res) => {
   // do your magic!
 });
-
 // custom middleware
-
 function validatePostId(req, res, next) {
   // do your magic!
 }
-
 module.exports = router;
